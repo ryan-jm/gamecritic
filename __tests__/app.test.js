f38b3f1008d5a719af4268e33869f0472128ac10
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-17 12:23:11 +0000

    /api/ health check tests & supertest install

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,22 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const seed = require('../db/seeds/seed.js');
+const app = require('../app');
+
+const request = require('supertest');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('API Endpoints', () => {
+  describe('GET: /api/ - Health Check', () => {
+    it('should respond with a 200 status code', () => {
+      return request(app).get('/api/').expect(200);
+    });
+
+    it('should respond with an "API Healthy" message', async () => {
+      const res = await request(app).get('/api/').expect(200);
+      expect(res.body.message).toBe('API Healthy');
+    });
+  });
+});
