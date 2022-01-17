d0970b3482d6e5d4b3b61182f95507efb8caa73c
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-17 13:04:52 +0000

    getReviewById model & controller

--- app/controllers/reviews/getReviewById.js
+++ app/controllers/reviews/getReviewById.js
@@ -1,5 +1,14 @@
 const { fetchReviewById } = require('../../models/reviews');
 
-const getReviewById = async (req, res, next) => {};
+const getReviewById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const review = await fetchReviewById(id);
+    return res.status(200).send({ review });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = getReviewById;
--- app/models/reviews/fetchReviewById.js
+++ app/models/reviews/fetchReviewById.js
@@ -1,5 +1,29 @@
+const format = require('pg-format');
 const db = require('../../../db/connection');
 
-const fetchReviewById = async () => {};
+const fetchReviewById = async (id) => {
+  console.log('This is called');
+  if (!id) throw new Error({ status: 400, message: 'Invalid ID provided' });
+
+  const query = `
+    SELECT reviews.owner, reviews.title, reviews.review_id, reviews.review_body, reviews.designer, 
+    reviews.review_img_url, reviews.category, reviews.created_at, reviews.votes, COUNT(comments.comment_id) AS comment_count
+    FROM reviews
+    JOIN comments 
+    ON comments.review_id = $1
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.owner, reviews.title, reviews.review_id, reviews.review_body, reviews.designer, 
+    reviews.review_img_url, reviews.category, reviews.created_at, reviews.votes`;
+
+  try {
+    const res = await db.query(query, [id]);
+    if (res.rows.length === 0)
+      throw new Error({ status: 404, message: 'No review found' });
+    else return res.rows[0];
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
 
 module.exports = fetchReviewById;
