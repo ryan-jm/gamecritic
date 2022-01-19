815665834c231c4ceec2f79d5f2503a0aa5e9ddf
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 11:38:28 +0000

    query clean-up

--- app/models/users/fetchAllUsers.js
+++ app/models/users/fetchAllUsers.js
@@ -2,7 +2,7 @@ const db = require('../../../db/connection');
 
 const fetchAllUsers = async () => {
   try {
-    const res = await db.query('SELECT * FROM users');
+    const res = await db.query('SELECT * FROM users;');
     return res.rows;
   } catch (err) {
     return Promise.reject(err);
