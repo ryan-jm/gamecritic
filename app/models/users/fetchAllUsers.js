f00563f2eeb6cea9b21185b2fc6c6db43a2e8dde
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 11:28:52 +0000

    fetchAllUsers model

---
+++ app/models/users/fetchAllUsers.js
@@ -0,0 +1,12 @@
+const db = require('../../../db/connection');
+
+const fetchAllUsers = async () => {
+  try {
+    const res = await db.query('SELECT * FROM users');
+    return res.rows;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+module.exports = fetchAllUsers;
