d533b1f21733ae8ba88365daab85915be4059ef7
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 10:22:45 +0000

    update exports

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,4 @@
 exports.idValidator = require('./idValidator');
 exports.reviewValidator = require('./reviewValidator');
 exports.commentValidator = require('./commentValidator');
+exports.categoryValidator = require('./categoryValidator');
