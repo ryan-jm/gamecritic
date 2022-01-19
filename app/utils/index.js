5b6fad5699239cb6d32236e64959c66052958adf
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 10:57:49 +0000

    export userValidator

--- app/utils/index.js
+++ app/utils/index.js
@@ -2,3 +2,4 @@ exports.idValidator = require('./idValidator');
 exports.reviewValidator = require('./reviewValidator');
 exports.commentValidator = require('./commentValidator');
 exports.categoryValidator = require('./categoryValidator');
+exports.userValidator = require('./userValidator');
