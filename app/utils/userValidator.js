9f776a583ef7a129f1855c643c828d5489fa2413
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 13:05:30 +0000

    minor logic change

--- app/utils/userValidator.js
+++ app/utils/userValidator.js
@@ -1,7 +1,12 @@
 const fetchAllUsers = require('../models/users/fetchAllUsers');
+const idValidator = require('./idValidator');
 
 const userValidator = async (username) => {
   if (typeof username !== 'string') return false;
+
+  const coercable = idValidator(username);
+  if (coercable) return false;
+
   const users = await fetchAllUsers();
   for (const user of users) {
     if (username === user.username) return 200;
