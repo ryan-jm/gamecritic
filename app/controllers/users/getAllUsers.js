05ba253abd60519ba629484ddaba9ac1db0c4de9
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 11:38:06 +0000

    getAllUsers controller

--- app/controllers/users/getAllUsers.js
+++ app/controllers/users/getAllUsers.js
@@ -3,6 +3,7 @@ const { fetchAllUsers } = require('../../models/users');
 const getAllUsers = async (req, res, next) => {
   try {
     const users = await fetchAllUsers();
+    res.status(200).send({ users });
   } catch (err) {
     return next(err);
   }
