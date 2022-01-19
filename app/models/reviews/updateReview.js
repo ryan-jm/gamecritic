d07148d5737494fd5a11fcc9ff00037f31c89f97
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 10:07:15 +0000

    logic update for inc_vote validation

--- app/models/reviews/updateReview.js
+++ app/models/reviews/updateReview.js
@@ -2,11 +2,11 @@ const db = require('../../../db/connection');
 const format = require('pg-format');
 const { reviewValidator, idValidator } = require('../../utils');
 
-const updateReview = async ({ inc_votes }, id) => {
+const updateReview = async ({ inc_votes = 0 }, id) => {
   const voteInputValid = idValidator(inc_votes); // Confirms is a number or can be coerced to a number
   const reviewIdValid = await reviewValidator(id);
 
-  if (!reviewIdValid || !voteInputValid) {
+  if (!reviewIdValid || (!voteInputValid && inc_votes !== 0)) {
     let status, message;
 
     if (!reviewIdValid)
