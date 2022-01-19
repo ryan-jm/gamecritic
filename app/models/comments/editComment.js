6f91aad4f8e4f8ba99092dc1e166f44a8b0e63f8
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 13:42:43 +0000

    editComment model

---
+++ app/models/comments/editComment.js
@@ -0,0 +1,36 @@
+const commentValidator = require('../../utils/commentValidator');
+const idValidator = require('../../utils/idValidator');
+const format = require('pg-format');
+
+const editComment = async ({ inc_votes }, id) => {
+  const commentValid = await commentValidator(id);
+  const votesValid = idValidator(inc_votes);
+
+  if (!votesValid) {
+    return Promise.reject({ status: 400, message: 'Invalid inc_votes value' });
+  }
+
+  if (!commentValid) {
+    return Promise.reject({ status: 400, message: 'Invalid comment id' });
+  } else if (commentValid === 404) {
+    return Promise.reject({ status: 404, message: 'Comment does not exist' });
+  } else {
+    const query = format(
+      `
+      UPDATE comments
+      SET votes = votes + %L
+      WHERE comments.comment_id = %L
+      RETURNING *;`,
+      inc_votes,
+      id
+    );
+    try {
+      const res = await db.query(query);
+      return res.rows[0];
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+};
+
+module.exports = editComment;
