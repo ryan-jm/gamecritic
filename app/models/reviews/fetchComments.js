f1c1346c524b2d6475f91481273ef02b2ff4cb65
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 10:48:47 +0000

    error handling logic for review ids

--- app/models/reviews/fetchComments.js
+++ app/models/reviews/fetchComments.js
@@ -5,6 +5,8 @@ const fetchComments = async (id) => {
   const isValid = await reviewValidator(id);
   if (!isValid) {
     return Promise.reject({ status: 400, message: 'Invalid review id' });
+  } else if (isValid === 404) {
+    return Promise.reject({ status: 404, message: 'Review cannot be found' });
   } else {
     try {
       const res = await db.query(
@@ -12,9 +14,7 @@ const fetchComments = async (id) => {
         WHERE comments.review_id = $1`,
         [id]
       );
-      if (res.rows.length === 0)
-        return Promise.reject({ status: 404, message: 'No comments found' });
-      else return res.rows;
+      return res.rows;
     } catch (err) {
       return Promise.reject(err);
     }
