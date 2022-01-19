649cdf96daaeb5d9ccd19f886d8a80dcb6b9fcb1
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-19 11:46:21 +0000

    remove unnecessary logs

--- app/controllers/users/getAllUsers.js
+++ app/controllers/users/getAllUsers.js
@@ -3,7 +3,8 @@ const { fetchAllUsers } = require('../../models/users');
 const getAllUsers = async (req, res, next) => {
   try {
     const users = await fetchAllUsers();
-    res.status(200).send({ users });
+    console.log(users);
+    return res.status(200).send({ users });
   } catch (err) {
     return next(err);
   }
