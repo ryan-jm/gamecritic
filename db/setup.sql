762fc3cbe8050286e66d8ca4996ef70dcdd33710
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-21 14:41:50 +0000

    minor changes to db naming

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS gamecritic;
-DROP DATABASE IF EXISTS gamecritic;
+DROP DATABASE IF EXISTS gamecritic_test;
 
-CREATE DATABASE gamecritic_test;
+CREATE DATABASE gamecritic;
 CREATE DATABASE gamecritic_test;
