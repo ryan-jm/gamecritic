9c7f84cebf3bd9359233e4908c88b0dc41463325
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-21 15:48:38 +0000

    typo fix

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -5,7 +5,8 @@ const { getAuthInfo, postAuthInfo } = require('../../controllers/auth');
 const authRateLimit = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 10,
-  handler = (req, res, next) => next({status: 429, message: 'Rate limit exceeded'}),
+  handler: (req, res, next) =>
+    next({ status: 429, message: 'Rate limit exceeded' }),
 });
 
 authRouter.use(authRateLimit);
