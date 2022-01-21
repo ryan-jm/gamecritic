25fef98f7199c7bb6b7c282ac8ab743a7c08afb4
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-21 15:46:31 +0000

    rate limit auth requests

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -1,5 +1,14 @@
 const authRouter = require('express').Router();
+const rateLimit = require('express-rate-limit');
 const { getAuthInfo, postAuthInfo } = require('../../controllers/auth');
+
+const authRateLimit = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  handler = (req, res, next) => next({status: 429, message: 'Rate limit exceeded'}),
+});
+
+authRouter.use(authRateLimit);
 
 authRouter.route('/').get(getAuthInfo).post(postAuthInfo);
 
