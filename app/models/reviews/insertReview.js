00fdc3d5b6a92413f99d6e14ee8b3102bb09b478
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-22 13:10:36 +0000

    insertReview model

--- app/models/reviews/insertReview.js
+++ app/models/reviews/insertReview.js
@@ -1,4 +1,4 @@
-const { userValidator } = require('../../utils');
+const validator = require('../../utils');
 const db = require('../../../db/connection');
 
 const insertReview = async (body) => {
@@ -8,9 +8,32 @@ const insertReview = async (body) => {
 
   const { owner, title, review_body, designer, category } = body;
 
-  const userValid = await userValidator(owner);
+  const userValid = await validator.userValidator(owner);
+  const categoryValid = await validator.categoryValidator(category);
+
   if (userValid === 404) {
     return Promise.reject({ status: 400, message: 'User not found' });
+  }
+
+  if (!categoryValid) {
+    return Promise.reject({ status: 400, message: 'Category invalid' });
+  }
+
+  try {
+    const { rows } = await db.query(
+      `
+      INSERT INTO reviews
+      (title, review_body, designer, category, owner)
+      VALUES
+      ($1, $2, $3, $4, $5)
+      RETURNING *;
+      `,
+      [title, review_body, designer, category, owner]
+    );
+    if (rows.length !== 0) return rows[0];
+    else return Promise.reject({ status: 408, message: 'POST failed.' });
+  } catch (err) {
+    return Promise.reject(err);
   }
 };
 
