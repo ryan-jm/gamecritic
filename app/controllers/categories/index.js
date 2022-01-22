c914c2ae0fdae79ecff05c14590448115cdfb431
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-22 14:19:24 +0000

    postCategory controller setup

--- app/controllers/categories/index.js
+++ app/controllers/categories/index.js
@@ -1,9 +1,22 @@
-const { fetchAllCategories } = require('../../models/categories');
+const {
+  fetchAllCategories,
+  insertCategory,
+} = require('../../models/categories');
 
 exports.getAllCategories = async (req, res, next) => {
   try {
     const categories = await fetchAllCategories();
     return res.status(200).send({ categories });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.postCategory = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const category = await insertCategory(body);
+    if (category) return res.status(201).send({ category });
   } catch (err) {
     return next(err);
   }
