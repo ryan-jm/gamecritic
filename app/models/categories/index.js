720a21a6498eb768db9044baaf62f09dc5d88dfb
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-22 14:24:08 +0000

    insertCategory model

--- app/models/categories/index.js
+++ app/models/categories/index.js
@@ -1,3 +1,4 @@
+const validator = require('../../utils');
 const db = require('../../../db/connection');
 
 exports.fetchAllCategories = async () => {
@@ -10,7 +11,33 @@ exports.fetchAllCategories = async () => {
 };
 
 exports.insertCategory = async (body) => {
+  const { slug, description } = body;
+
+  if (!slug) {
+    return Promise.reject({ status: 400, message: 'Invalid or missing slug' });
+  }
+
+  if (!description) {
+    return Promise.reject({
+      status: 400,
+      message: 'Invalid or missing description',
+    });
+  }
+
+  const categoryExists = await validator.categoryValidator(slug);
+  if (categoryExists) {
+    return Promise.reject({ status: 400, message: 'Category already exists' });
+  }
+
   try {
+    const { rows } = await db.query(`
+    INSERT INTO categories
+    (slug, description)
+    VALUES
+    ($1, $2)
+    RETURNING *;
+    `);
+    if (rows) return rows[0];
   } catch (err) {
     return Promise.reject(err);
   }
