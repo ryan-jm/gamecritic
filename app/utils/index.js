b0beff31b35587a46d592bedaa0eb3a1812a2edc
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-22 12:32:03 +0000

    convert validators to a class Validator

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,61 +1,83 @@
-const fetchAllReviews = require('../models/reviews/fetchAllReviews');
-const fetchAllUsers = require('../models/users/fetchAllUsers');
 const db = require('../../db/connection');
 
-function idValidator(id) {
-  if (typeof id !== 'string' && typeof id !== 'number') return false;
-  return Boolean(parseInt(id));
-}
+class Validator {
+  constructor(db) {
+    this.db = db;
+  }
 
-async function reviewValidator(id) {
-  /* Check that the review_id is valid within the database */
-  try {
-    if (!idValidator(id)) return false;
-    else {
-      id = parseInt(id);
-      if (id < 0) return false;
-      const { reviews } = await fetchAllReviews({ limit: 1000 });
-      return id <= reviews.length ? 200 : 404;
+  idValidator(id) {
+    if (typeof id !== 'string' && typeof id !== 'number') return false;
+    return Boolean(parseInt(id));
+  }
+
+  async reviewValidator(id) {
+    /* Check that the review_id is valid within the database */
+    try {
+      if (!this.idValidator(id)) return false;
+      else {
+        id = parseInt(id);
+        if (id < 0) return false;
+        const { rows: reviews } = await this.db.query(
+          `SELECT review_id FROM reviews;`
+        );
+        return id <= reviews.length ? 200 : 404;
+      }
+    } catch (err) {
+      return false;
     }
-  } catch (err) {
+  }
+
+  async userValidator(username) {
+    if (typeof username !== 'string') return false;
+
+    const coercable = this.idValidator(username);
+    if (coercable) return false;
+
+    try {
+      const { rows: users } = await this.db.query(
+        `SELECT username FROM users;`
+      );
+      if (users) {
+        for (const user of users) {
+          if (username === user) return 200;
+        }
+      }
+    } catch (err) {
+      return false;
+    }
+
+    return 404;
+  }
+
+  async commentValidator(id) {
+    /* Check that the comment_id is valid within the database */
+
+    try {
+      if (!this.idValidator(id)) return false;
+      else {
+        id = parseInt(id);
+        if (id < 0) return false;
+        /* Will turn this into an endpoint using MVC pattern at some point */
+        const { rows: comments } = await db.query(`SELECT * FROM comments;`);
+        return id <= comments.length ? 200 : 404;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async categoryValidator(input) {
+    if (typeof input !== 'string') return false;
+    try {
+      const { rows: categories } = await db.query('SELECT * FROM categories;');
+      for await (const category of categories) {
+        if (input === category.slug) return true;
+      }
+    } catch (err) {
+      return false;
+    }
     return false;
   }
 }
 
-async function userValidator(username) {
-  if (typeof username !== 'string') return false;
-
-  const coercable = idValidator(username);
-  if (coercable) return false;
-
-  const users = await fetchAllUsers();
-  for (const user of users) {
-    if (username === user.username) return 200;
-  }
-
-  return 404;
-}
-
-async function commentValidator(id) {
-  /* Check that the comment_id is valid within the database */
-
-  try {
-    if (!idValidator(id)) return false;
-    else {
-      id = parseInt(id);
-      if (id < 0) return false;
-      /* Will turn this into an endpoint using MVC pattern at some point */
-      const { rows: comments } = await db.query(`SELECT * FROM comments;`);
-      return id <= comments.length ? 200 : 404;
-    }
-  } catch (err) {
-    return false;
-  }
-}
-
-module.exports = {
-  idValidator,
-  reviewValidator,
-  userValidator,
-  commentValidator,
-};
+module.exports = new Validator(db);
