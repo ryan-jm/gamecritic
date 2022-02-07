73b0b8140f6d36cefef8cb2ce08d838c51e8cab8
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-07 11:16:52 +0000

    cors middleware

--- app/index.js
+++ app/index.js
@@ -4,8 +4,8 @@ const { auth, errorHandler } = require('./middleware');
 const routes = require('./routes');
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(auth);
 app.use('/api', routes);
 app.use(errorHandler);
