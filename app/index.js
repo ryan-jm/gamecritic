bf025d0ea1ac038578bd78cc4450d94d1f744929
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-07 10:55:05 +0000

    allow cors

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { auth, errorHandler } = require('./middleware');
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(auth);
 app.use('/api', routes);
 app.use(errorHandler);
