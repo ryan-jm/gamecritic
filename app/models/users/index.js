f72ed442877eac6ac4f82c25118339aa940aeecf
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-08 22:29:10 +0000

    add insertVote and fetchVotes controllers

--- app/models/users/index.js
+++ app/models/users/index.js
@@ -1,5 +1,6 @@
 const db = require('../../../db/connection');
 const validator = require('../../utils');
+const { updateReview } = require('../reviews');
 
 exports.fetchAllUsers = async () => {
   try {
@@ -29,6 +30,85 @@ exports.fetchSingleUser = async (user) => {
         user,
       ]);
       return res.rows[0];
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+};
+
+exports.fetchVotes = async (username, type) => {
+  const isValid = await validator.userValidator(username);
+
+  if (!isValid) {
+    return Promise.reject({
+      status: 400,
+      message: 'Invalid username provided',
+    });
+  } else if (isValid === 404) {
+    return Promise.reject({
+      status: 404,
+      message: 'User does not exist',
+    });
+  } else {
+    try {
+      const res = await db.query(`SELECT * FROM reviewvotes WHERE owner = $1`, [
+        username,
+      ]);
+      return res.rows;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+};
+
+exports.insertVote = async (username, review) => {
+  const isValid = await validator.userValidator(username);
+
+  if (!isValid) {
+    return Promise.reject({
+      status: 400,
+      message: 'Invalid username provided',
+    });
+  } else if (isValid === 404) {
+    return Promise.reject({
+      status: 404,
+      message: 'User does not exist',
+    });
+  } else {
+    try {
+      await updateReview({ inc_votes: 1 }, review);
+      const res = await db.query(
+        `INSERT INTO reviewvotes (owner, review) VALUES ($1, $2) RETURNING *;`,
+        [username, review]
+      );
+      return res.rows[0];
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+};
+
+exports.deleteVote = async (username, review) => {
+  const isValid = await validator.userValidator(username);
+
+  if (!isValid) {
+    return Promise.reject({
+      status: 400,
+      message: 'Invalid username provided',
+    });
+  } else if (isValid === 404) {
+    return Promise.reject({
+      status: 404,
+      message: 'User does not exist',
+    });
+  } else {
+    try {
+      await db.query(
+        `DELETE FROM reviewvotes WHERE owner = $1 AND review = $2;`,
+        [username, review]
+      );
+      const res = await updateReview({ inc_votes: -1 }, review);
+      return res;
     } catch (err) {
       return Promise.reject(err);
     }
