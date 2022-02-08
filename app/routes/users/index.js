d572b9e21e36573a86f84cdf994fbc4e57271ed9
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-08 22:30:00 +0000

    setup users/votes endpoint

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,7 +1,13 @@
 const userRouter = require('express').Router();
-const { getAllUsers, getSingleUser } = require('../../controllers/users');
+const {
+  getAllUsers,
+  getSingleUser,
+  getUserVotes,
+  postVote,
+} = require('../../controllers/users');
 
 userRouter.get('/', getAllUsers);
 userRouter.route('/:username').get(getSingleUser);
+userRouter.route('/:username/votes').get(getUserVotes).post(postVote);
 
 module.exports = userRouter;
