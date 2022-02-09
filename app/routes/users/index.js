ee46ab5381886981b3dd58b02f96f3101077ca9f
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-09 10:49:39 +0000

    delete route for votes

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -4,10 +4,15 @@ const {
   getSingleUser,
   getUserVotes,
   postVote,
+  deleteVote,
 } = require('../../controllers/users');
 
 userRouter.get('/', getAllUsers);
 userRouter.route('/:username').get(getSingleUser);
-userRouter.route('/:username/votes').get(getUserVotes).post(postVote);
+userRouter
+  .route('/:username/votes')
+  .get(getUserVotes)
+  .post(postVote)
+  .delete(deleteVote);
 
 module.exports = userRouter;
